9ab985418d8f912ea7d50f64201b5b03d56b62e7
Jon Oddvar Rambjør <jonoddram@gmail.com>
2020-08-02 14:29:48 +0200

    Added functionally for selecting chat messages between specific timestamps

--- RechatTool/Program.cs
+++ RechatTool/Program.cs
@@ -9,20 +9,46 @@ using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
 
-namespace RechatTool {
-	internal class Program {
+namespace RechatTool
+{
+	internal class Program
+	{
 		public const string Version = "1.5.0.5";
 
-		private static int Main(string[] args) {
+		private static int Main(string[] args)
+		{
 			int iArg = 0;
 			string PeekArg() =>
 				iArg < args.Length ? args[iArg] : null;
 			string GetArg(bool optional = false) =>
 				iArg < args.Length ? args[iArg++] : optional ? (string)null : throw new InvalidArgumentException();
 
-			try {
+			try
+			{
 				string arg = GetArg();
-				if (arg == "-d" || arg == "-D") {
+				if (arg == "-t") // Gets a certain time interval from the JSON file given as an argument
+				{
+					// parse args
+					string inputPath = @GetArg();
+					string startTimeString = GetArg();
+					string endTimeString = GetArg();
+					long startTime = TryParseTimestamp(startTimeString);
+					long endTime = TryParseTimestamp(endTimeString);
+					string outputPath;
+					if (args.Length > 4) // if a output path is provided
+					{
+						outputPath = args[4]; // index start at 0
+					}
+					else
+					{
+						outputPath = Path.Combine(Path.GetDirectoryName(inputPath), Path.GetFileNameWithoutExtension(inputPath) +
+							"_" + startTimeString.Replace(":", "_") + "_" + endTimeString.Replace(":", "_") + ".json");
+					}
+					// actual code is done in Rechat
+					Rechat.ExtractChatTimestampwise(startTime, endTime, startTimeString, endTimeString, inputPath, outputPath);
+				}
+				else if (arg == "-d" || arg == "-D")
+				{ // Gets file from Twitch as JSON or as txt
 					bool processFile = arg == "-D";
 					string videoIdStr = GetArg();
 					long videoId = videoIdStr.TryParseInt64() ??
@@ -30,80 +56,102 @@ namespace RechatTool {
 						throw new InvalidArgumentException();
 					string path = PeekArg()?.StartsWith("-", StringComparison.Ordinal) == false ? GetArg() : $"{videoId}.json";
 					bool overwrite = false;
-					while ((arg = GetArg(true)) != null) {
-						if (arg == "-o") {
+					while ((arg = GetArg(true)) != null)
+					{
+						if (arg == "-o")
+						{
 							overwrite = true;
 						}
-						else {
+						else
+						{
 							throw new InvalidArgumentException();
 						}
 					}
-					void UpdateProgress(int segmentCount, TimeSpan? contentOffset) {
+					void UpdateProgress(int segmentCount, TimeSpan? contentOffset)
+					{
 						string message = $"Downloaded {segmentCount} segment{(segmentCount == 1 ? "" : "s")}";
-						if (contentOffset != null) {
+						if (contentOffset != null)
+						{
 							message += $", offset = {Rechat.TimestampToString(contentOffset.Value, false)}";
 						}
 						Console.Write($"\r{message}");
 					}
-					try {
+					try
+					{
 						Rechat.DownloadFile(videoId, path, overwrite, UpdateProgress);
 						Console.WriteLine();
 					}
-					catch (WarningException ex) {
+					catch (WarningException ex)
+					{
 						Console.WriteLine();
 						Console.WriteLine($"Warning: {ex.Message}");
 					}
-					if (processFile) {
-						try {
+					if (processFile)
+					{
+						try
+						{
 							Console.WriteLine("Processing file");
 							Rechat.ProcessFile(path, overwrite: overwrite);
 						}
-						catch (WarningException ex) {
+						catch (WarningException ex)
+						{
 							Console.WriteLine($"Warning: {ex.Message}");
 						}
 					}
 					Console.WriteLine("Done!");
 				}
-				else if (arg == "-p") {
+				else if (arg == "-p")
+				{
 					string[] paths = { GetArg() };
 					string outputPath = null;
-					if (paths[0].IndexOfAny(new[] { '*', '?'}) != -1) {
+					if (paths[0].IndexOfAny(new[] { '*', '?' }) != -1)
+					{
 						paths = Directory.GetFiles(Path.GetDirectoryName(paths[0]), Path.GetFileName(paths[0]));
 					}
-					else if (PeekArg()?.StartsWith("-", StringComparison.Ordinal) == false) {
+					else if (PeekArg()?.StartsWith("-", StringComparison.Ordinal) == false)
+					{
 						outputPath = GetArg();
 					}
 					bool overwrite = false;
 					bool showBadges = false;
-					while ((arg = GetArg(true)) != null) {
-						if (arg == "-o") {
+					while ((arg = GetArg(true)) != null)
+					{
+						if (arg == "-o")
+						{
 							overwrite = true;
 						}
-						else if (arg == "-b") {
+						else if (arg == "-b")
+						{
 							showBadges = true;
 						}
-						else {
+						else
+						{
 							throw new InvalidArgumentException();
 						}
 					}
-					foreach (string p in paths) {
+					foreach (string p in paths)
+					{
 						Console.WriteLine($"Processing {Path.GetFileName(p)}");
-						try {
+						try
+						{
 							Rechat.ProcessFile(p, pathOut: outputPath, overwrite: overwrite, showBadges: showBadges);
 						}
-						catch (WarningException ex) {
+						catch (WarningException ex)
+						{
 							Console.WriteLine($"Warning: {ex.Message}");
 						}
 					}
 					Console.WriteLine("Done!");
 				}
-				else {
+				else
+				{
 					throw new InvalidArgumentException();
 				}
 
 				return 0;
 			}
-			catch (InvalidArgumentException) {
+			catch (InvalidArgumentException)
+			{
 				Console.WriteLine($"RechatTool v{new Version(Version).ToDisplayString()}");
 				Console.WriteLine();
 				Console.WriteLine("Modes:");
@@ -121,17 +169,27 @@ namespace RechatTool {
 				Console.WriteLine("            extension changed to .txt.");
 				Console.WriteLine("        -o: Overwrite the existing output file. ");
 				Console.WriteLine("        -b: Show user badges (e.g. moderator/subscriber).");
+				Console.WriteLine("   -t input_path [start_timestamp] [end_timestamp] [output_path]");
+				Console.WriteLine("      Creates new file output_path (default: original input_path_start_timestamp_end_timestamp.json from a json file created by this program containing\n" +
+								  "      only chat messages from the time interval starTimestamp to endTimestamp (inclusive)\n" +
+								  "         input_path: The path to the json file you want to convert from. Should be absolute, not relative. \n" +
+								  "         start_timestamp: The start time of the chat interval you want to extract. Inclusive.\n (format hh:mm:ss" +
+								  "         end_timestamp: The end time of the chat interval you want to extract. Inclusive.\n" +
+								  "         output_path: The output path, absolute not relative. Default is original filename_start_timestamp_end_timestamp.json.");
 				return 1;
 			}
-			catch (Exception ex) {
+			catch (Exception ex)
+			{
 				Console.WriteLine("\rError: " + ex.Message);
 				return 1;
 			}
 		}
 
-		private static long? TryParseVideoIdFromUrl(string s) {
+		private static long? TryParseVideoIdFromUrl(string s)
+		{
 			string[] hosts = { "twitch.tv", "www.twitch.tv" };
-			if (!s.StartsWith("http", StringComparison.OrdinalIgnoreCase)) {
+			if (!s.StartsWith("http", StringComparison.OrdinalIgnoreCase))
+			{
 				s = "https://" + s;
 			}
 			if (!Uri.TryCreate(s, UriKind.Absolute, out Uri uri)) return null;
@@ -139,6 +197,26 @@ namespace RechatTool {
 			Match match = Regex.Match(uri.AbsolutePath, "^/(videos|[^/]+/video)/(?<videoId>[0-9]+)$");
 			if (!match.Success) return null;
 			return match.Groups["videoId"].Value.TryParseInt64();
+		}
+
+		private static long TryParseTimestamp(string s)
+		{
+			try
+			{
+				long hours;
+				long minutes;
+				long seconds;
+				long.TryParse(s.Substring(0, 2), out hours);
+				long.TryParse(s.Substring(3, 2), out minutes);
+				long.TryParse(s.Substring(6, 2), out seconds);
+				seconds = hours * 60 * 60 + minutes * 60 + seconds;
+				return seconds;
+			}
+			catch (InvalidArgumentException)
+			{
+				Console.WriteLine("Invalid timestamp format. Correct format: hh:mm:ss. Example: 02:12:00");
+				throw new System.ArgumentException("Invalid timestamp format. Correct format: hh:mm:ss. Example: 02:12:00");
+			}
 		}
 
 		private class InvalidArgumentException : Exception { }
--- RechatTool/Rechat.cs
+++ RechatTool/Rechat.cs
@@ -13,10 +13,14 @@ using System.Text;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
-namespace RechatTool {
-	public static class Rechat {
-		public static void DownloadFile(long videoId, string path, bool overwrite = false, DownloadProgressCallback progressCallback = null) {
-			if (File.Exists(path) && !overwrite) {
+namespace RechatTool
+{
+	public static class Rechat
+	{
+		public static void DownloadFile(long videoId, string path, bool overwrite = false, DownloadProgressCallback progressCallback = null, int? timeStampStart = null, int? timeStampEnd = null)
+		{
+			if (File.Exists(path) && !overwrite)
+			{
 				throw new Exception("Output file already exists.");
 			}
 			string baseUrl = $"{"https"}://api.twitch.tv/v5/videos/{videoId}/comments";
@@ -24,14 +28,17 @@ namespace RechatTool {
 			int segmentCount = 0;
 			JObject firstComment = null;
 			JObject lastComment = null;
-			using (var writer = new JsonTextWriter(new StreamWriter(path, false, new UTF8Encoding(true)))) {
+			using (var writer = new JsonTextWriter(new StreamWriter(path, false, new UTF8Encoding(true))))
+			{
 				writer.WriteStartArray();
-				do {
+				do
+				{
 					string url = nextCursor == null ?
 						$"{baseUrl}?content_offset_seconds=0" :
 						$"{baseUrl}?cursor={nextCursor}";
 					JObject response = JObject.Parse(DownloadUrlAsString(url, withRequest: AddTwitchApiHeaders));
-					foreach (JObject comment in (JArray)response["comments"]) {
+					foreach (JObject comment in (JArray)response["comments"])
+					{
 						comment.WriteTo(writer);
 						firstComment = firstComment ?? comment;
 						lastComment = comment;
@@ -43,92 +50,113 @@ namespace RechatTool {
 				while (nextCursor != null);
 				writer.WriteEndArray();
 			}
-			if (firstComment != null) {
-				try {
+			if (firstComment != null)
+			{
+				try
+				{
 					var firstMessage = new RechatMessage(firstComment);
 					var lastMessage = new RechatMessage(lastComment);
 					File.SetCreationTimeUtc(path, firstMessage.CreatedAt - firstMessage.ContentOffset);
 					File.SetLastWriteTimeUtc(path, lastMessage.CreatedAt);
 				}
-				catch (Exception ex) {
+				catch (Exception ex)
+				{
 					throw new WarningException("Unable to set file created/modified time.", ex);
 				}
 			}
 		}
 
-		private static string DownloadUrlAsString(string url, Action<HttpWebRequest> withRequest = null) {
+		private static string DownloadUrlAsString(string url, Action<HttpWebRequest> withRequest = null)
+		{
 			HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
 			withRequest?.Invoke(request);
 			using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
-			using (StreamReader responseStream = new StreamReader(response.GetResponseStream())) {
+			using (StreamReader responseStream = new StreamReader(response.GetResponseStream()))
+			{
 				return responseStream.ReadToEnd();
 			}
 		}
 
-		private static void AddTwitchApiHeaders(HttpWebRequest request) {
+		private static void AddTwitchApiHeaders(HttpWebRequest request)
+		{
 			request.Accept = "application/vnd.twitchtv.v5+json";
 			request.Headers.Add("Client-ID", "jzkbprff40iqj646a697cyrvl0zt2m6");
 		}
 
-		private static TimeSpan? TryGetContentOffset(JObject comment) {
-			try {
+		private static TimeSpan? TryGetContentOffset(JObject comment)
+		{
+			try
+			{
 				return comment == null ? (TimeSpan?)null : new RechatMessage(comment).ContentOffset;
 			}
-			catch {
+			catch
+			{
 				return null;
 			}
 		}
 
-		public static void ProcessFile(string pathIn, string pathOut = null, bool overwrite = false, bool showBadges = false) {
-			if (pathOut == null) {
+		public static void ProcessFile(string pathIn, string pathOut = null, bool overwrite = false, bool showBadges = false)
+		{
+			if (pathOut == null)
+			{
 				bool isAlreadyTxt = pathIn.EndsWith(".txt", StringComparison.OrdinalIgnoreCase);
 				pathOut = Path.Combine(
 					Path.GetDirectoryName(pathIn),
 					Path.GetFileNameWithoutExtension(pathIn) + (isAlreadyTxt ? "-p" : "") + ".txt");
 			}
-			if (File.Exists(pathOut) && !overwrite) {
+			if (File.Exists(pathOut) && !overwrite)
+			{
 				throw new Exception("Output file already exists.");
 			}
 			IEnumerable<string> lines = ParseMessages(pathIn)
 				.Select(n => ToReadableString(n, showBadges))
 				.Where(n => n != null);
 			File.WriteAllLines(pathOut, lines, new UTF8Encoding(true));
-			try {
+			try
+			{
 				File.SetCreationTimeUtc(pathOut, File.GetCreationTimeUtc(pathIn));
 				File.SetLastWriteTimeUtc(pathOut, File.GetLastWriteTimeUtc(pathIn));
 			}
-			catch (Exception ex) {
+			catch (Exception ex)
+			{
 				throw new WarningException("Unable to set file created/modified time.", ex);
 			}
 		}
 
-		public static IEnumerable<RechatMessage> ParseMessages(string path) {
-			using (var reader = new JsonTextReader(File.OpenText(path))) {
-				while (reader.Read()) {
+		public static IEnumerable<RechatMessage> ParseMessages(string path)
+		{
+			using (var reader = new JsonTextReader(File.OpenText(path)))
+			{
+				while (reader.Read())
+				{
 					if (reader.TokenType != JsonToken.StartObject) continue;
 					yield return new RechatMessage(JObject.Load(reader));
 				}
 			}
 		}
 
-		public static string TimestampToString(TimeSpan value, bool showMilliseconds) {
+		public static string TimestampToString(TimeSpan value, bool showMilliseconds)
+		{
 			return $"{(int)value.TotalHours:00}:{value:mm}:{value:ss}{(showMilliseconds ? $".{value:fff}" : "")}";
 		}
 
-		private static string ToReadableString(RechatMessage m, bool showBadges) {
+		private static string ToReadableString(RechatMessage m, bool showBadges)
+		{
 			string userBadges = $"{(m.UserIsAdmin || m.UserIsStaff ? "*" : "")}{(m.UserIsBroadcaster ? "#" : "")}{(m.UserIsModerator || m.UserIsGlobalModerator ? "@" : "")}{(m.UserIsSubscriber ? "+" : "")}";
 			string userName = m.UserDisplayName.Equals(m.UserName, StringComparison.OrdinalIgnoreCase) ? m.UserDisplayName : $"{m.UserDisplayName} ({m.UserName})";
 			return $"[{TimestampToString(m.ContentOffset, true)}] {(showBadges ? userBadges : "")}{userName}{(m.IsAction ? "" : ":")} {m.MessageText}";
 		}
 
-		public class RechatMessage {
+		public class RechatMessage
+		{
 			public JObject SourceJson { get; }
 
 			private JsonComment Comment { get; }
 			private JsonCommentCommenter Commenter => Comment.Commenter;
 			private JsonCommentMessage Message => Comment.Message;
 
-			public RechatMessage(JObject sourceJson) {
+			public RechatMessage(JObject sourceJson)
+			{
 				SourceJson = sourceJson;
 				Comment = sourceJson.ToObject<JsonComment>();
 			}
@@ -165,7 +193,8 @@ namespace RechatTool {
 
 			private bool HasBadge(string id) => Message.UserBadges?.Any(n => n.Id.Equals(id, StringComparison.OrdinalIgnoreCase)) ?? false;
 
-			private class JsonComment {
+			private class JsonComment
+			{
 				[JsonProperty("created_at")]
 				public DateTime CreatedAt { get; set; }
 				[JsonProperty("content_offset_seconds")]
@@ -178,14 +207,16 @@ namespace RechatTool {
 				public JsonCommentMessage Message { get; set; }
 			}
 
-			private class JsonCommentCommenter {
+			private class JsonCommentCommenter
+			{
 				[JsonProperty("display_name")]
 				public string DisplayName { get; set; }
 				[JsonProperty("name")]
 				public string Name { get; set; }
 			}
 
-			private class JsonCommentMessage {
+			private class JsonCommentMessage
+			{
 				[JsonProperty("body")]
 				public string Body { get; set; }
 				[JsonProperty("is_action")]
@@ -194,21 +225,25 @@ namespace RechatTool {
 				public JsonCommentUserBadge[] UserBadges { get; set; }
 			}
 
-			private class JsonCommentUserBadge {
+			private class JsonCommentUserBadge
+			{
 				[JsonProperty("_id")]
 				public string Id { get; set; }
 				[JsonProperty("version")]
 				public int Version { get; set; }
 
-				public UserBadge ToUserBadge() {
-					return new UserBadge {
+				public UserBadge ToUserBadge()
+				{
+					return new UserBadge
+					{
 						Id = Id,
 						Version = Version
 					};
 				}
 			}
 
-			public class UserBadge {
+			public class UserBadge
+			{
 				internal UserBadge() { }
 
 				public string Id { get; internal set; }
@@ -217,5 +252,25 @@ namespace RechatTool {
 		}
 
 		public delegate void DownloadProgressCallback(int segmentCount, TimeSpan? contentOffset);
+
+		public static void ExtractChatTimestampwise(long startTime, long endTime, string startTimeString, string endTimeString, string inputPath, string outputPath)
+		{
+			string json = File.ReadAllText(inputPath);
+			dynamic jsonObj = Newtonsoft.Json.JsonConvert.DeserializeObject(json);
+			using (StreamWriter file = File.CreateText(outputPath))
+			using (JsonTextWriter writer = new JsonTextWriter(file))
+			{
+				writer.WriteRaw("[");
+				for (int i0 = 0; i0 < jsonObj.Count; i0++)
+				{
+					if (jsonObj[i0].content_offset_seconds >= startTime && jsonObj[i0].content_offset_seconds <= endTime)
+					{
+						jsonObj[i0].WriteTo(writer);
+					}
+					writer.WriteRaw(",");
+				}
+				writer.WriteRaw("]");
+			}
+		}
 	}
 }
